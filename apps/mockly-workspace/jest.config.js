e0b37aa5da56b3d890a047b947806bfdb76eb103
Pablo Lazaro <pablo.lazaro@gft.com>
2019-07-15 15:51:17 +0200

    chore(workspace): deletes all apps

--- apps/mockly-workspace/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'mockly-workspace',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/mockly-workspace',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
