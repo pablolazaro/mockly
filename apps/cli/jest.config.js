2614fd5d8743b4821a2dc67073b4b2bb8c83b835
Pablo Lazaro <pablo.lazaro@gft.com>
2019-07-17 19:58:10 +0200

    build(codecov): adds coverage reports

--- apps/cli/jest.config.js
+++ apps/cli/jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   name: 'server',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/apps/cli',
-
   reporters: ["default", ["jest-junit", {outputDirectory: './reports/apps/cli'}]]
 };
