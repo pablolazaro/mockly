b1ba6b6c82a2ad474ad0fcca80743091d494e625
Pablo Lazaro <pablo.lazaro@gft.com>
2019-07-18 12:33:21 +0200

    test(server): adds configuration tests

---
+++ apps/server/src/utils/configuration.spec.ts
@@ -0,0 +1,32 @@
+import { getConfiguration, getConfigurationValidationErrors } from './configuration';
+import { DEFAULT_CONFIG } from '../constants';
+import { MocklyConfig } from '../models';
+
+describe('Configuration utils', () => {
+
+  it('should get default configuration', async () => {
+    const config = await getConfiguration();
+
+    expect(config.delay).toEqual(DEFAULT_CONFIG.delay);
+    expect(config.port).toEqual(DEFAULT_CONFIG.port);
+    expect(config.prefix).toEqual(DEFAULT_CONFIG.prefix);
+    expect(config.resourceFilesGlob).toEqual(DEFAULT_CONFIG.resourceFilesGlob);
+    expect(config.responsesConfigGlob).toEqual(DEFAULT_CONFIG.responsesConfigGlob);
+    expect(config.rewritesFilesGlob).toEqual(DEFAULT_CONFIG.rewritesFilesGlob);
+  });
+
+  it('should validate a valid configuration', async () => {
+    const config = await getConfiguration();
+    const errors = await getConfigurationValidationErrors(config);
+
+    expect(errors).toHaveLength(0);
+  });
+
+  it('should validate an invalid configuration', async () => {
+    const config = new MocklyConfig(    700, 3000, 1 as any, null, null, null);
+    const errors = await getConfigurationValidationErrors(config);
+
+    expect(errors).toHaveLength(1)
+  });
+
+});
