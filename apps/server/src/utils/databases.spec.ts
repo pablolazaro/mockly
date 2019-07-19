17bb703105a2df589992f24ae5f6c84b66bf0cd9
Pablo Lazaro <pablo.lazaro@gft.com>
2019-07-19 12:50:04 +0200

    tests(server): adds database and resource utils tests

--- apps/server/src/utils/databases.spec.ts
+++ apps/server/src/utils/databases.spec.ts
@@ -1,12 +1,29 @@
-import { createDatabase } from './databases';
+import { createDatabase, hydrateDatabase } from './databases';
+import PouchDB from 'pouchdb';
 
 describe('Databases utils', () => {
+  let db: PouchDB.Database;
+
+  beforeAll(() => {
+    db = createDatabase('test');
+  });
+
+  afterAll(() => {
+    db.destroy();
+  });
+
   describe('createDatabase', () => {
     it('should create a database', () => {
-      const db = createDatabase('test');
-
       expect(db).toBeDefined();
       expect(db.name).toBe('test');
+    });
+  });
+
+  describe('hydrateDatabase', () => {
+    it('should hydrate a database', async () => {
+      await hydrateDatabase(db, [{ name: 'Kitty ' }]);
+      const result = await db.allDocs();
+      expect(result.rows).toHaveLength(1);
     });
   });
 });
--- apps/server/src/utils/resources.spec.ts
+++ apps/server/src/utils/resources.spec.ts
@@ -1,6 +1,11 @@
-import { appendPrefix } from './common';
-import { getDefinitions, getResourceFiles, isRestResource } from './resources';
+import {
+  createAndHydrateJsonDatabase,
+  createAndHydrateResourcesDatabases,
+  getDefinitions,
+  isRestResource
+} from './resources';
 import { ResourceType } from '../models/resource-type';
+import { ResourceDefinition } from '../models/resource-definition';
 
 describe('Resources utils', () => {
   describe('getDefinitions', () => {
@@ -46,6 +51,45 @@ describe('Resources utils', () => {
       expect(isRestResource([1, 2, 3])).toBe(false);
       expect(isRestResource(['1', '2', '3'])).toBe(false);
       expect(isRestResource([true, false, true])).toBe(false);
+    });
+  });
+
+  describe('createAndHydrateJsonDatabase', () => {
+    it('should create an hydrate database', async () => {
+      const resources = [
+        {
+          name: 'me',
+          type: ResourceType.JSON_DATA,
+          resources: { name: 'Peter' }
+        }
+      ] as ResourceDefinition[];
+
+      const db = await createAndHydrateJsonDatabase(resources);
+
+      expect(db).toBeDefined();
+      expect(db.name).toBe('data');
+
+      expect((await db.allDocs()).rows).toHaveLength(1);
+    });
+  });
+
+  describe('createAndHydrateResourcesDatabases', () => {
+    it('should create an hydrates resources database', async () => {
+      const resources = [
+        {
+          name: 'artists',
+          type: ResourceType.REST_RESOURCE,
+          resources: [{ name: 'Bon Jovi' }, { name: 'Georgie Dann' }]
+        }
+      ] as ResourceDefinition[];
+
+      const map = await createAndHydrateResourcesDatabases(resources);
+
+      expect(map).toBeDefined();
+      expect(map.has('artists')).toBe(true);
+      expect(map.get('artists')).toBeDefined();
+      expect(map.get('artists').name).toBe('artists');
+      expect((await map.get('artists').allDocs()).rows).toHaveLength(2);
     });
   });
 });
