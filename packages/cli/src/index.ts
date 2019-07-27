c81c60d4d6e357efc2e8ba8b94ea186720dff911
Pablo Lazaro <pablolp90@gmail.com>
2019-07-27 22:29:24 +0200

    feat(cli): adds flags and improvements

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,4 +1,5 @@
-import { Command } from '@oclif/command';
+import { Command, flags } from '@oclif/command';
+import cli from 'cli-ux';
 // tslint:disable-next-line:nx-enforce-module-boundaries
 import {
   createAndHydrateJsonDatabase,
@@ -20,11 +21,56 @@ import {
   RewriteConfig,
   start
 } from '@mockly/server';
+import { join } from 'path';
+import * as tsNode from 'ts-node';
+import { cwd } from 'process';
 
 const CFonts = require('cfonts');
 
 export class MocklyCli extends Command {
-  static description = 'describe the command here';
+  static description = 'starts Mockly server';
+
+  static flags = {
+    tsConfig: flags.string({
+      description: 'path to TS config file',
+      dependsOn: ['controllers'],
+      helpValue: '**/*.my-controller.ts'
+    }),
+    controllers: flags.string({
+      description: 'glob to retrieve custom controllers files',
+      default: '**/*.custom-controller.ts',
+      helpValue: '**/*.my-controller.ts'
+    }),
+    resources: flags.string({
+      description: 'glob to retrieve resource files',
+      default: '**/*.resource.json',
+      helpValue: '**/*.db.json'
+    }),
+    rewrites: flags.string({
+      description: 'glob to retrieve rewrites files',
+      default: '**/*.rewrites.json',
+      helpValue: '**/*.proxy.json'
+    }),
+    responses: flags.string({
+      description: 'glob to retrieve responses files',
+      default: '**/*.responses.json',
+      helpValue: '**/*.responses.json'
+    }),
+    prefix: flags.string({
+      description: 'prefix',
+      helpValue: '/api'
+    }),
+    delay: flags.integer({
+      description: 'sets a delay for all responses',
+      default: 0,
+      helpValue: '500'
+    }),
+    port: flags.integer({
+      description: 'server port',
+      default: 3100,
+      helpValue: '3200'
+    })
+  };
 
   async run() {
     this.printTitle('Mockly');
@@ -33,34 +79,75 @@ export class MocklyCli extends Command {
       'Welcome to Mockly! You will have your favourite mock server running soon!\n'
     );
 
-    const config = await this.getAndValidateConfiguration();
+    cli.action.start('Fetching Mockly configuration');
+    const options = this.parse(MocklyCli).flags;
+    const configFromOptions = this.getConfigFromOptions(options);
+    const config = await this.getAndValidateConfiguration(configFromOptions);
+    cli.action.stop();
 
+    cli.action.start('Fetching responses configuration');
     const responsesConfig = await this.getAndValidateResponsesConfiguration(
       config.responsesConfigGlob
     );
+    cli.action.stop();
 
+    cli.action.start('Fetching rewrites');
     const rewrites = await this.getAndValidateRewritesConfiguration(
       config.rewritesFilesGlob
     );
+    cli.action.stop();
 
+    cli.action.start('Fetching resources and data');
     const definitions = await getResourcesAndDataDefinitions(
       config.resourceFilesGlob
     );
+    cli.action.stop();
 
+    cli.action.start('Fetching custom controllers');
+
+    let customControllers = [];
+
+    try {
+      const tsConfigPath = options.tsConfig;
+
+      if (tsConfigPath) {
+        tsNode.register({
+          project: join(cwd(), tsConfigPath)
+        });
+      }
+
+      customControllers = await getCustomControllers(
+        config.customControllersGlob
+      );
+    } catch (error) {
+      this.warn(
+        'An error occurred importing a custom controller. If you are creating custom controllers you should pass a tsconfig file!'
+      );
+      this.error(error);
+    }
+
+    cli.action.stop();
+
+    cli.action.start('Creating databases');
     const dbsMap = await this.getDatabasesMap(definitions, responsesConfig);
+    cli.action.stop();
 
+    cli.action.start('Creating resources and data controllers');
     const controllers = getControllers(definitions, config.prefix);
+    cli.action.stop();
 
-    const customControllers = await getCustomControllers(
-      config.customControllersGlob
-    );
+    cli.action.start('Starting the server');
 
-    return await start(
+    const app = await start(
       [...customControllers, ...controllers],
       dbsMap,
       config,
       rewrites
     );
+
+    cli.action.stop();
+
+    return app;
   }
 
   printTitle(title: string) {
@@ -76,15 +163,30 @@ export class MocklyCli extends Command {
     });
   }
 
-  async getAndValidateConfiguration(): Promise<MocklyConfig> {
+  getConfigFromOptions(options: any): MocklyConfig {
+    return new MocklyConfig(
+      options.controllers,
+      options.delay,
+      options.port,
+      options.prefix,
+      options.resources,
+      options.rewrites,
+      options.responses
+    );
+  }
+
+  async getAndValidateConfiguration(
+    configFromOptions: MocklyConfig
+  ): Promise<MocklyConfig> {
     const config = await getConfiguration();
-    const errors = await getConfigurationValidationErrors(config);
+    const mergedConfig = { ...config, ...configFromOptions };
+    const errors = await getConfigurationValidationErrors(mergedConfig);
 
     if (errors.length === 0) {
-      return config;
+      return mergedConfig;
     } else {
       this.error('Invalid configuration file!');
-      return config;
+      return mergedConfig;
     }
   }
 
