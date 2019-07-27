168f773700d83f906a1e10e855d8f80b215c44b7
Pablo Lázaro <pablolp90@gmail.com>
2019-07-27 23:04:01 +0200

    chore(mockly): refactor packages

--- packages/server/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'server',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/packages/server',
-  reporters: [
-    'default',
-    ['jest-junit', { outputDirectory: './reports/packages/server' }]
-  ]
-};
